54b8f020a898a17a5c22e48d162c76a1f2981607
jail <125803424+mini-jail@users.noreply.github.com>
2023-02-20 07:18:27 +0000

    init, innit?

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # gpl-to-json
 converts GIMP Palettes to JSON
+
+# structure
+```
+Color {
+  name: string
+  hex: string
+  red: number
+  green: number
+  blue: number
+}
+
+ColorPalette {
+  prefix: string
+  name: string
+  columns: number
+  colors: Color[]
+}
+```
+
+# usage
+## write output to json file
+```
+deno run --allow-read --allow-write https://raw.githubusercontent.com/mini-jail/gpl-to-json/main/mod.ts input.gpl output.json
+```
+
+## write output to console
+```
+deno run --allow-read --allow-write https://raw.githubusercontent.com/mini-jail/gpl-to-json/main/mod.ts input.gpl
+```
+
+```
+deno run --allow-read --allow-write https://raw.githubusercontent.com/mini-jail/gpl-to-json/main/mod.ts input.gpl > ./output.json
+```
+
+# motivation
+i need it, lol
---
+++ mod.ts
@@ -0,0 +1,55 @@
+const [input = null, output = null] = Deno.args
+
+if (input === null) {
+  console.error("argument missing: input")
+  Deno.exit(1)
+}
+
+const [_prefix, _name, _columns, ..._colors] = (await Deno.readTextFile(input))
+  .split("\n")
+  .filter((line) => !line.startsWith("#"))
+
+export type Color = {
+  name: string
+  hex: string
+  red: number
+  green: number
+  blue: number
+}
+
+export type ColorPalette = {
+  prefix: string
+  name: string
+  columns: number
+  colors: Color[]
+}
+
+const colorPalette: ColorPalette = {
+  prefix: String(_prefix),
+  name: String(_name),
+  columns: Number(_columns.split(":")[1]),
+  colors: _colors.map((color) => {
+    const [_red, _green, _blue, ..._name] = color.split(" ")
+    const name = _name.join(" ").trim().replace(/ +/g, " ")
+    const red = Number(_red)
+    const green = Number(_green)
+    const blue = Number(_blue)
+    return {
+      name,
+      hex: `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`,
+      red,
+      green,
+      blue,
+    }
+  }),
+}
+
+const colorPaletteJSON = JSON.stringify(colorPalette, null, 2)
+
+if (output) {
+  await Deno.writeTextFile(output, colorPaletteJSON)
+}
+
+if (output === null) {
+  console.log(colorPaletteJSON)
+}
